c6e53d2cb674261f184ea59de177197b7260e65f
Demonorium <Demonorium@yandex.ru>
2026-01-03 09:55:40 +0300

    version: 4

---
+++ push_update.py
@@ -0,0 +1,37 @@
+from git import Repo
+
+with open('modpack_version.txt', mode='r', encoding='utf-8') as f:
+    version = int(f.readline())
+version += 1
+with open('modpack_version.txt', mode='w', encoding='utf-8') as f:
+    f.write(str(version))
+
+print('new version:', version)
+
+repo = Repo('./')
+print('repo initiated:', repo.common_dir)
+print('index...')
+repo.index.add('modpack_version.txt')
+repo.index.add('push_update.py')
+repo.index.add('install_update.py')
+repo.index.add('check_update.py')
+
+repo.index.add('config/*')
+repo.index.add('kubejs/assets/*')
+repo.index.add('kubejs/client_scripts/*')
+repo.index.add('kubejs/config/**')
+repo.index.add('kubejs/server_scripts/*')
+repo.index.add('kubejs/data/*')
+repo.index.add('kubejs/startup_scripts/*')
+repo.index.add('defaultconfigs/*')
+repo.index.add('keybinding presets/*')
+repo.index.add('minemenu/*')
+
+print('commit...')
+repo.index.commit('version: ' + str(version))
+print('remote search')
+origin = repo.remote(name='origin')
+print('remote found')
+print('push...')
+origin.push()
+print('success')
