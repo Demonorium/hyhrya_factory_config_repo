f0e6b477ea319f0edead7320c7a362054acb9fb7
Demonorium <Demonorium@yandex.ru>
2026-01-03 14:21:45 +0300

    version: 23

--- kubejs/server_scripts/loot/dump.js
+++ kubejs/server_scripts/loot/dump.js
@@ -1,19 +1,19 @@
 // priority: 0
 
-ServerEvents.loaded(event => {
-    const $LootDataType = Java.loadClass('net.minecraft.world.level.storage.loot.LootDataType')
+// ServerEvents.loaded(event => {
+//     const $LootDataType = Java.loadClass('net.minecraft.world.level.storage.loot.LootDataType')
     
-    let lootData = Utils.server.getLootData()
-    // this will be a list of all the resourcelocations.
-    let allTables = lootData.getKeys($LootDataType.TABLE)
+//     let lootData = Utils.server.getLootData()
+//     // this will be a list of all the resourcelocations.
+//     let allTables = lootData.getKeys($LootDataType.TABLE)
     
     
-    // filter it for only stuff that contains chest
-    let filteredList = allTables.stream()
-        .filter(id => !id.path.contains('chest') && !id.path.contains('block'))
-        .map(id => [id.toString(), JSON.stringify(lootData.getElement($LootDataType.TABLE, id))])
-        .toList()
+//     // filter it for only stuff that contains chest
+//     let filteredList = allTables.stream()
+//         .filter(id => !id.path.contains('chest') && !id.path.contains('block'))
+//         .map(id => [id.toString(), JSON.stringify(lootData.getElement($LootDataType.TABLE, id))])
+//         .toList()
     
-    JsonIO.write('kubejs/exported/lootTables.json', {big_list: filteredList})
+//     JsonIO.write('kubejs/exported/lootTables.json', {big_list: filteredList})
     
-})
+// })
