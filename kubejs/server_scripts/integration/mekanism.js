5d8244a91851044fd75837d32fb74ab4da84f04a
Demonorium <Demonorium@yandex.ru>
2026-01-03 16:26:49 +0300

    version: 25

--- kubejs/server_scripts/integration/mekanism.js
+++ kubejs/server_scripts/integration/mekanism.js
@@ -7,7 +7,16 @@ const mekanism_extras_levels = ['absolute', 'supreme', 'cosmic', 'infinite']
 ServerEvents.recipes(event => {
   console.log("meka recipes loading...")
 
-  event.replaceInput({id: 'mekanism:upgrade/'}, 'glass_pane', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism:upgrade/anchor'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism:upgrade/energy'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism:upgrade/energy'}, MATERIALS.GOLD.dust, 'superbwarfare:cell')
+  event.replaceInput({id: 'mekanism:upgrade/filter'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism:upgrade/gas'}, 'glass', 'pneumaticcraft:volume_upgrade')
+  event.replaceInput({id: 'mekanism:upgrade/muffling'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism:upgrade/speed'}, 'glass', 'pneumaticcraft:speed_upgrade')
+  event.replaceInput({id: 'mekanism:upgrade/stone_generator'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism_extras:upgrade/filter'}, 'glass', '#pneumaticcraft:upgrade_components')
+  event.replaceInput({id: 'mekanism_extras:upgrade/stack'}, 'glass', 'thermal:upgrade_augment_3')
 
 
   event.remove({id: 'thermal:machines/pulverizer/pulverizer_cobblestone'})
@@ -37,10 +46,26 @@ ServerEvents.recipes(event => {
   event.replaceInput({id: 'bfr:reactor/port'}, 'mekanism:ultimate_control_circuit', 'mekanism_extras:absolute_control_circuit')
   event.replaceInput({id: 'bfr:laser_focus_matrix'}, 'redstone_block', 'mffs:focus_matrix')
 
-  event.replaceInput({id: "thermal:augments/upgrade_augment_1"}, "glass", "mekanism:advanced_control_circuit")
-
-  event.replaceInput({id: "thermal:augments/upgrade_augment_1"}, "glass", "mekanism:advanced_control_circuit")
-  event.replaceInput({id: "thermal:augments/upgrade_augment_2"}, "thermal:signalum_gear", "mekanism:elite_control_circuit")
+  event.replaceInput({id: "thermal:augments/upgrade_augment_1"}, "glass", "mekanism:upgrade_speed")
+  event.replaceInput({id: "thermal:augments/upgrade_augment_2"}, "thermal:signalum_gear", "mekanism:advanced_control_circuit")
+  event.replaceInput({id: "thermal:augments/upgrade_augment_3"}, "thermal:lumium_gear", "mekanism:elite_control_circuit")
+  event.replaceInput({id: "thermalendergy:endergy_upgrade_1"}, "thermal:enderium_gear", "mekanism:ultimate_control_circuit")
+  event.replaceInput({id: "thermalendergy:endergy_upgrade_2"}, "thermalendergy:prismalium_gear", "mekanism_extras:absolute_control_circuit")
+  event.replaceInput({id: "thermalendergy:endergy_upgrade_3"}, "thermalendergy:melodium_gear", "mekanism_extras:supreme_control_circuit")
+  event.remove({id: 'thermalendergy:vibrating_core'})
+  event.shaped(
+    Item.of('thermalendergy:vibrating_core'),
+    [
+      "ADA",
+      "CBC",
+      "ADA",
+    ], {
+      'A': MATERIALS.RUTHENIUM.plate,
+      "C": MATERIALS.AZURE_ELECTRUM.plate,
+      "D": MATERIALS.REGALIUM.ingot,
+      "B": Item.of('deeperdarker:reinforced_echo_shard')
+    }
+  )
 
   event.custom({
     "type": "pneumaticcraft:thermo_plant",
