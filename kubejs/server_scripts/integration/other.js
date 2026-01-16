1603c32b43a74487d55718ef5b12e1d9fd283045
Demonorium <Demonorium@yandex.ru>
2026-01-16 22:21:38 +0300

    version: 86

--- kubejs/server_scripts/integration/other.js
+++ kubejs/server_scripts/integration/other.js
@@ -22,7 +22,7 @@ ServerEvents.recipes(event => {
   event.replaceInput({ mod: 'mffs' }, "diamond", 'mekanism:ultimate_control_circuit')
   event.replaceInput({ mod: 'mffs' }, "redstone", 'pncepcb:crystal_clear_finished_pcb')
   event.replaceInput({ mod: 'mffs' }, "ender_pearl", 'mekanism_extras:cosmic_control_circuit')
-  event.replaceInput({ id: 'undergarden:catalyst' }, 'dimaond', 'meetyourfight:phantoplasm')
+  event.replaceInput({ id: 'undergarden:catalyst' }, 'diamond', 'meetyourfight:phantoplasm')
   event.replaceInput({ mod: 'wirelesschargers' }, 'iron_ingot', MATERIALS.STEEL.plate)
   event.replaceInput({ mod: 'wirelesschargers' }, 'blaze_powder', 'mekanism:advanced_control_circuit')
   event.replaceInput({ mod: 'wirelesschargers' }, 'redstone_block', 'thermal:machine_frame')
