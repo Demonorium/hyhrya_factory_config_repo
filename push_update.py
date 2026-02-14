a945c8c84e4dbefd32be3029628535394b9b16b5
Demonorium <Demonorium@yandex.ru>
2026-02-14 12:06:11 +0300

    version: 159

--- push_update.py
+++ push_update.py
@@ -1,4 +1,34 @@
 from git import Repo
+import urllib.error as error
+import urllib.request
+import json
+import os
+
+hooks = {}
+if os.path.exists('webhooks.json'):
+    with open('webhooks.json', 'r', encoding='utf-8') as f:
+        hooks = json.load(f)
+   
+def send_message_to_hook(hook, message):
+    if hook not in hooks:
+        print('no hook:', hook)
+        return
+    
+    hook_url = hooks[hook]
+    try:
+        json_data = {
+            'text': message,
+            'username': "Скрипт установки"
+        }
+
+        req = urllib.request.Request(hook_url, data=json.dumps(json_data).encode('utf-8'), method='POST')
+        req.add_header("Content-Type", "application/json")
+        with urllib.request.urlopen(req) as response:
+            response_data = response.read().decode('utf-8')
+            print("Hook sent:", hook, response_data)
+
+    except Exception as error:
+        print('Error in hook request:', error)
 
 with open('modpack_version.txt', mode='r', encoding='utf-8') as f:
     version = int(f.readline())
