95b9225178ecdd3881c4ca1ecf3b3109cb270314
Demonorium <Demonorium@yandex.ru>
2026-03-15 19:27:44 +0300

    version: 219

---
+++ update_scripts/218.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    install_lib.delete_mods(
+        'Mekanism-1.20.1-10.4.15.jar',
+    )
+
+    install_lib.simple_install(
+        ['Mekanism-1.20.1-10.4.15.homebaked.jar', 'Меканизм (кастомная версия)']
+    )
